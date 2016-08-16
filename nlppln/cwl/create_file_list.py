73db3c15cc32df92d3e4dd5b37fe8466b9a74165
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-08-16 16:50:22 +0200

    Add click command that creates an input file list for cwl
    Most cwl steps require a file list as input (instead of a list of
    file names). This is a convenience command that creates a
    parameter input file for all files in a directory. Having such a
    parameter file makes it easier to test individual cwl steps.

---
+++ nlppln/cwl/create_file_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import click
+import os
+import codecs
+import json
+
+
+@click.command()
+@click.argument('param_name')
+@click.argument('input_dir', type=click.Path(exists=True))
+@click.argument('output_file', type=click.Path())
+def command(param_name, input_dir, output_file):
+    """Generate a json file that specifies an input file list for a cwl step.
+    """
+    output_dir = os.path.dirname(output_file)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    json_out = {}
+    json_out[param_name] = []
+
+    files = os.listdir(input_dir)
+    for fi in files:
+        full_name = os.path.join(input_dir, fi)
+        json_out[param_name].append({'class': 'File', 'path': full_name})
+
+    with codecs.open(output_file, 'wb', encoding='utf-8') as f:
+        json.dump(json_out, f, indent=4)
+
+if __name__ == '__main__':
+    command()
