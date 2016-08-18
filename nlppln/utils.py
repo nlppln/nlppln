d2c135e3f4dccd77e95315a3f756f047e5a0ab65
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-08-18 22:34:39 +0200

    Add utility module with testable utiliy functionality

---
+++ nlppln/utils.py
@@ -0,0 +1,21 @@
+"""NLP pipeline utility functionality"""
+import os
+
+def remove_ext(fname):
+    """Removes the extension from a filename
+    """
+    bn = os.path.basename(fname)
+    return os.path.splitext(bn)[0]
+
+
+def create_dirs(fname):
+    """Create (output) directories if they don't exist
+    """
+    fname = os.path.dirname(fname)
+
+    if not os.path.exists(fname):
+        os.makedirs(fname)
+
+def out_file_name(out_dir, fname, ext):
+    fname = remove_ext(fname)
+    return os.path.join(out_dir, '{}.{}'.format(fname, ext))
