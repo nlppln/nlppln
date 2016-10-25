743ee552c07529795acec31671573d2829a4a5bf
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-10-25 16:54:28 +0200

    Add scriptcwl as dependency
    However, there is a problem with installing nlppln, see #10

--- setup.py
+++ setup.py
@@ -61,9 +61,10 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['amqp<2.0,>=1.4.9', 'cython', 'scipy', 'xtas', 'pandas',
+    install_requires=['amqp<2.0,>=1.4.9', 'cython', 'scipy', 'xtas',
                       'Flask==0.11.1', 'Flask-Bootstrap==3.3.6.0',
-                      'Flask-Bower==1.2.1', 'docx2txt'],
+                      'Flask-Bower==1.2.1', 'docx2txt', 'pandas'],
+    dependency_links=['git://github.com/NLeSC/scriptcwl.git@9530be254809cb7d4243e09df056a170ae1c9610#egg=scriptcwl']
 
     #scripts=['recipy-cmd']
 )
