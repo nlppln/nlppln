d489342a669fea5e4db2dc789767c9f9b4269f29
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-11-08 19:21:35 +0100

    Add Python command and CWL step to convert files to txt
    Files are converted using Apache Tika.

---
+++ nlppln/apachetika.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import click
+import codecs
+import tika
+from tika import parser
+
+from nlppln.utils import create_dirs, out_file_name
+
+
+@click.command()
+@click.argument('in_files', nargs=-1, type=click.Path(exists=True))
+@click.argument('out_dir', nargs=1, type=click.Path())
+def command(in_files, out_dir):
+    create_dirs(out_dir)
+
+    for fi in in_files:
+        parsed = parser.from_file(fi)
+
+        out_file = out_file_name(out_dir, fi, 'txt')
+        with codecs.open(out_file, 'wb', encoding='utf-8') as f:
+            f.write(parsed['content'])
+
+
+if __name__ == '__main__':
+    command()
