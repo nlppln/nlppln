011dfb524d8b1277a6fb405669aef91eb0d53443
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-12-20 11:45:57 +0100

    Add function to copy cwl files to default location
    cwl files are copied to $XDG_DATA_HOME/commonwl/ This is one of the
    default locations where the cwl-runner looks for cwl files.

--- nlppln/__init__.py
+++ nlppln/__init__.py
@@ -1,3 +1,8 @@
+import os
+
 from wfgenerator import WorkflowGenerator
 
 __version__ = '0.0.0'
+
+MODULE_PATH = os.path.dirname(os.path.realpath(__file__))
+CWL_PATH = os.path.abspath(os.path.join(MODULE_PATH, '../cwl'))
