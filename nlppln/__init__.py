c95f62c249a9c5eb8f63dbb3a7ce9afdc9c82422
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2017-02-27 16:43:26 +0100

    Update version to 0.1.0

--- nlppln/__init__.py
+++ nlppln/__init__.py
@@ -3,7 +3,7 @@ import os
 from wfgenerator import WorkflowGenerator
 from commands import *
 
-__version__ = '0.0.0'
+__version__ = '0.1.0'
 
 MODULE_PATH = os.path.dirname(os.path.realpath(__file__))
 CWL_PATH = os.path.abspath(os.path.join(MODULE_PATH, '../cwl'))
