fed7da9783c8d7375c77fd77974a7846dc9ba5cb
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2017-03-30 14:13:30 +0200

    Add python command and cwl step for doing LIWC

---
+++ cwl/liwc.cwl
@@ -0,0 +1,24 @@
+#!/usr/bin/env cwlrunner
+cwlVersion: cwl:v1.0
+class: CommandLineTool
+baseCommand: ["python", "-m", "nlppln.commands.liwc"]
+
+inputs:
+  liwc_dict:
+    type: File
+    inputBinding:
+      position: 1
+  in_files:
+    type: File[]
+    inputBinding:
+      position: 2
+  encoding:
+    type: string?
+    inputBinding:
+      prefix: -e
+
+outputs:
+  liwc:
+    type: File
+    outputBinding:
+      glob: "liwc.csv"
---
+++ nlppln/commands/liwc.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+import click
+import codecs
+import re
+import os
+import pandas as pd
+import numpy as np
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def load_liwc_dict(dict_file, encoding):
+
+    with codecs.open(dict_file, 'rb', encoding) as f:
+        lines = f.readlines()
+
+    liwc_categories = {}
+    liwc_dict = {}
+
+    for line in lines:
+        # LIWC category
+        if line[0].isdigit():
+            entry = line.split()
+            # remove 0 from strings like 01
+            c = str(int(entry[0]))
+            liwc_categories[c] = entry[1]
+        # word
+        elif line[0].isalpha():
+            entry = line.split()
+            term = entry[0]
+            categories = entry[1:]
+            liwc_dict[term] = categories
+    return liwc_dict, liwc_categories
+
+
+def liwc_word2re(word):
+    word = word.replace('*', '.*')
+    return re.compile(r'\b{}\b'.format(word.encode('utf-8')))
+
+
+@click.command()
+@click.argument('liwc_dict', type=click.Path(exists=True))
+@click.argument('in_files', nargs=-1, type=click.Path(exists=True))
+@click.option('--encoding', '-e', default='latin1',
+              help='Encoding of LIWC dictionary.')
+def command(liwc_dict, in_files, encoding):
+    liwc, liwc_categories = load_liwc_dict(liwc_dict, encoding)
+
+    text_ids = [os.path.basename(fi) for fi in in_files]
+    cols = liwc_categories.values()
+    result = pd.DataFrame(0, index=text_ids, columns=cols)
+
+    # make vocabulary
+    count_vect = CountVectorizer(input='filename')
+    word_counts = count_vect.fit_transform(in_files)
+
+    # get total number of words per text
+    result['#words'] = word_counts.sum(axis=1)
+
+    # get words that match the liwc regexes (these should be added to the
+    # vocabluary)
+    liwc_regexes = [w for w in liwc.keys() if '*' in w]
+    for w in liwc_regexes:
+        # remove last character (*) from liwc entry
+        start = w[0:len(w)-1]
+        for word in count_vect.vocabulary_:
+            if word.startswith(start):
+                liwc[word] = liwc[w]
+        del liwc[w]
+
+    # get frequencies for all liwc words
+    count_vect = CountVectorizer(input='filename', vocabulary=liwc.keys())
+    word_counts = count_vect.fit_transform(in_files)
+
+    # sum over categories
+    # http://stackoverflow.com/questions/37013115/how-to-read-traverse-slice-scipy-sparse-matrices-lil-csr-coo-dok-faster
+    for word, categories in liwc.iteritems():
+        w_idx = count_vect.vocabulary_.get(word)
+        col = np.zeros(len(in_files))
+        A = word_counts.getcol(w_idx).tocoo()
+        for i, j, d in zip(A.row, A.col, A.data):
+            col[i] = d
+
+        for cat in categories:
+            result[liwc_categories[cat]] += col
+
+    lc = liwc_categories.values()
+    result[lc] = result[lc].div(result['#words'], axis='index')
+    result[lc] = result[lc]*100.0
+
+    result.to_csv('liwc.csv', encoding='utf-8')
+
+
+if __name__ == '__main__':
+    command()
