f59dc5ed6416d6e189b5dab4a43edf0652c5c2b9
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2017-08-04 19:54:06 +0200

    Fix code style

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -4,8 +4,8 @@ from nlppln.generate import to_bool
 
 
 def test_to_bool_correct():
-    assert to_bool('y') == True
-    assert to_bool('n') == False
+    assert to_bool('y') is True
+    assert to_bool('n') is False
 
 
 def test_to_bool_error():
