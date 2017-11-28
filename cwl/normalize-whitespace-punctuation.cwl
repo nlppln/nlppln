0cd958757ad56d9e4ec7c37c8509611c6fca4c3a
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2017-11-28 13:51:25 +0100

    Add command and step to normalize whitespace and punctuation
    for a directory of text files.

---
+++ cwl/normalize-whitespace-punctuation.cwl
@@ -0,0 +1,16 @@
+#!/usr/bin/env cwlrunner
+cwlVersion: v1.0
+class: CommandLineTool
+baseCommand: ["python", "-m", "nlppln.commands.normalize_whitespace_punctuation"]
+
+inputs:
+  meta_in:
+    type: File
+    inputBinding:
+      position: 1
+
+outputs:
+  metadata_out:
+    type: File
+    outputBinding:
+      glob: "*.txt"
---
+++ nlppln/commands/normalize_whitespace_punctuation.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import click
+import codecs
+import os
+import re
+
+from string import punctuation
+
+from nlppln.utils import create_dirs, out_file_name
+
+
+def normalize_whitespace(text):
+    return u' '.join(text.split())
+
+
+def normalize_punctuation(text):
+    for c in punctuation:
+        try:
+            if c == u'.':
+                text = re.sub(r'\{}+'.format(c), c, text)
+            else:
+                text = re.sub(r'{}+'.format(c), c, text)
+        except:
+            if c != u'\\':
+                text = re.sub(r'\{}+'.format(c), c, text)
+    return text
+
+
+@click.command()
+@click.argument('txt', type=click.File(encoding='utf-8'))
+@click.option('--out_dir', '-o', default=os.getcwd(), type=click.Path())
+def normalize_whitespace_punctuation(txt, out_dir):
+    create_dirs(out_dir)
+
+    text = txt.read()
+    text = normalize_whitespace(text)
+    text = normalize_punctuation(text)
+
+    out_file = out_file_name(out_dir, os.path.basename(txt.name))
+    print out_file
+    with codecs.open(out_file, 'wb', encoding='utf-8') as f:
+        f.write(text)
+
+
+if __name__ == '__main__':
+    normalize_whitespace_punctuation()
