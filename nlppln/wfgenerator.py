55fd840b06c5481ff5e3171dba1ef98d973f0747
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2017-12-07 17:01:51 +0100

    Add working_dir and copy_steps options

--- nlppln/wfgenerator.py
+++ nlppln/wfgenerator.py
@@ -4,8 +4,9 @@ from .utils import CWL_PATH
 
 
 class WorkflowGenerator(WFGenerator):
-    def __init__(self):
-        WFGenerator.__init__(self, steps_dir=CWL_PATH)
+    def __init__(self, working_dir=None, copy_steps=True):
+        WFGenerator.__init__(self, steps_dir=CWL_PATH, working_dir=working_dir,
+                             copy_steps=copy_steps)
 
     def save(self, fname, inline=True, relative=False, validate=True,
              encoding='utf-8'):
