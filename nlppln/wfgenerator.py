82740c7956a2bae0baceedd658b9ad9352254ad0
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2017-12-12 14:29:18 +0100

    Update to use newest (unreleased) scriptcwl options

--- nlppln/wfgenerator.py
+++ nlppln/wfgenerator.py
@@ -4,18 +4,20 @@ from .utils import CWL_PATH
 
 
 class WorkflowGenerator(WFGenerator):
-    def __init__(self, working_dir=None, copy_steps=True):
-        WFGenerator.__init__(self, steps_dir=CWL_PATH, working_dir=working_dir,
-                             copy_steps=copy_steps)
+    def __init__(self, working_dir=None):
+        WFGenerator.__init__(self, steps_dir=CWL_PATH, working_dir=working_dir)
 
-    def save(self, fname, inline=True, relative=False, validate=True,
-             encoding='utf-8'):
+    def save(self, fname, validate=True, wd=True, inline=False, relative=False,
+             pack=False, encoding='utf-8'):
         """Save workflow to file
 
-        For nlppln, the default is to save steps inline.
+        For nlppln, the default is to use a working directory (and save steps
+        using the ``wd`` option).
         """
         super(WorkflowGenerator, self).save(fname,
+                                            validate=validate,
+                                            wd=wd,
                                             inline=inline,
                                             relative=relative,
-                                            validate=validate,
+                                            pack=pack,
                                             encoding=encoding)
