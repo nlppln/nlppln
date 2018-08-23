5453842c05e13cbe745b202c540f273c85afa097
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2018-08-23 19:23:53 +0200

    Update version
    Prepare new release.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Change Log
 
-## Unreleased
+## 0.3.2
 
 ### Added
 
--- nlppln/__init__.py
+++ nlppln/__init__.py
@@ -1,4 +1,4 @@
 from .wfgenerator import WorkflowGenerator
 from .commands import *
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
